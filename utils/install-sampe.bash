71a3d04523894e69c66c32c478b7091483eb71bd
Takashi Mori <kanetugu2015@gmail.com>
2023-07-30 14:26:50 +0900

    add tb3

--- utils/install-sampe.bash
+++ utils/install-sampe.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cp config/hakoniwa_path.json hakoniwa-unity-tb3model/plugin/plugin-srcs/hakoniwa_path.json 
+cp -rp workspace/dev/tb3 hakoniwa-ros2pdu/workspace/src/
+mv hakoniwa-ros2pdu/workspace/src/tb3/build-tb3.bash hakoniwa-ros2pdu/workspace/
---
+++ workspace/runtime/tb3-run.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /root/workspace/hakoniwa-ros2pdu/workspace
+source install/setup.bash
+ros2 run tb3 tb3_node
