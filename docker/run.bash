f95705968e9e5048d9755873564abb2a7a3d46cc
Takashi Mori <kanetugu2015@gmail.com>
2023-07-30 13:57:14 +0900

    add run for dev

--- docker/run.bash
+++ docker/run.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source docker/env.bash
+
 HAKONIWA_TOP_DIR=`pwd`
 IMAGE_NAME=`cat docker/image_name.txt`
 IMAGE_TAG=`cat docker/appendix/latest_version.txt`
@@ -7,7 +9,6 @@ DOCKER_IMAGE=${IMAGE_NAME}:${IMAGE_TAG}
 
 
 OS_TYPE=`bash utils/detect_os_type.bash`
-
 if [ ${OS_TYPE} != "Mac" ]
 then
 	docker ps > /dev/null
@@ -21,7 +22,9 @@ fi
 
 if [ ${OS_TYPE} = "wsl2" ]
 then
-	IPADDR=`cat /etc/resolv.conf  | grep nameserver | awk '{print $NF}'`
+	export RESOLV_IPADDR=`cat /etc/resolv.conf  | grep nameserver | awk '{print $NF}'`
+	NETWORK_INTERFACE=$(route | grep '^default' | grep -o '[^ ]*$' | tr -d '\n')
+	CORE_IPADDR=$(ifconfig "${NETWORK_INTERFACE}" | grep netmask | awk '{print $2}')
 elif [ ${OS_TYPE} = "Mac" ]
 then
 	if [ $# -ne 1 ]
@@ -29,24 +32,22 @@ then
 		echo "Usage: $0 <port>"
 		exit 1
 	fi
-	ETHER=${1}
-	IPADDR=`ifconfig | grep -A1 ${ETHER} | grep netmask | awk '{print $2}'`
+	#NETWORK_INTERFACE=$(netstat -rnf inet | grep '^default' | awk '{print $4}')
+	#CORE_IPADDR=$(ifconfig "${NETWORK_INTERFACE}" | grep netmask | awk '{print $2}')
+	CORE_IPADDR="127.0.0.1"
 else
 	IPADDR="127.0.0.1"
 fi
 
-if [ ${OS_TYPE} != "Mac" ]
-then
-	docker run -v ${HAKONIWA_TOP_DIR}:/root/workspace/hakoniwa-ros2bridge-dev \
+
+docker run \
+		-v `pwd`:/root/workspace \
 		-it --rm \
 		--net host \
-		-e CORE_IPADDR=${IPADDR} \
-		-e OS_TYPE=${OS_TYPE} \
+		-e CORE_IPADDR=${CORE_IPADDR} \
+		-e DELTA_MSEC=${DELTA_MSEC} \
+		-e MAX_DELAY_MSEC=${MAX_DELAY_MSEC} \
+		-e GRPC_PORT=${GRPC_PORT} \
+		-e UDP_SRV_PORT=${UDP_SRV_PORT} \
+		-e UDP_SND_PORT=${UDP_SND_PORT} \
 		--name hakoniwa-ros2bridge-dev-container ${DOCKER_IMAGE} 
-else
-	docker run -v ${HAKONIWA_TOP_DIR}:/root/workspace/hakoniwa-ros2bridge-dev \
-		-it --rm \
-		-e CORE_IPADDR=${IPADDR} \
-		-e OS_TYPE=${OS_TYPE} \
-		--name hakoniwa-ros2bridge-dev-container ${DOCKER_IMAGE} 
-fi
---
+++ workspace/runtime/run.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#export DELTA_MSEC=20
+#export MAX_DELAY_MSEC=100
+#export GRPC_PORT=50051
+#export UDP_SRV_PORT=54001
+#export UDP_SND_PORT=54002
+#export CORE_IPADDR=172.30.224.33
+ASSET_DEF=asset_def.txt
+grep mqtt ${ASSET_DEF} > /dev/null
+HAS_MQTT=$?
+echo "ASSET_DEF=${ASSET_DEF}"
+if [ ${HAS_MQTT} -eq 0 ]
+then
+    echo "INFO: ACTIVATING MOSQUITTO"
+    mosquitto -c ./config/mosquitto.conf &
+    MQTT_PORT=1883
+    sleep 2
+else
+    MQTT_PORT=
+fi
+
+
+echo "INFO: ACTIVATING HAKO-CONDUCTOR"
+hako-master ${DELTA_MSEC} ${MAX_DELAY_MSEC} ${CORE_IPADDR}:${GRPC_PORT} ${UDP_SRV_PORT} ${UDP_SND_PORT} ${MQTT_PORT} & > /dev/null 
+
+sleep 1
+
+LAST_PID=
+for entry in `cat ${ASSET_DEF}`
+do
+    echo "INFO: ACTIVATING $entry"
+    sleep 1
+done
+
+while [ 1 ]
+do
+    sleep 10
+done
