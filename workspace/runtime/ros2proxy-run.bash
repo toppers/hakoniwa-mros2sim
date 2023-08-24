f50778beb52b49a0f4f5e02171d9037cc009721f
Takashi Mori <kanetugu2015@gmail.com>
2023-08-24 10:28:27 +0900

    run.bash is customized for mros2

--- workspace/runtime/ros2proxy-run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /root/workspace/hakoniwa-ros2pdu/workspace
-source install/setup.bash
-ros2 run hako_pdu_proxy hako_pdu_proxy_node
--- workspace/runtime/run.bash
+++ workspace/runtime/run.bash
@@ -1,46 +1,98 @@
 #!/bin/bash
 
-#export DELTA_MSEC=20
-#export MAX_DELAY_MSEC=100
-#export GRPC_PORT=50051
-#export UDP_SRV_PORT=54001
-#export UDP_SND_PORT=54002
-#export CORE_IPADDR=172.30.224.33
-ASSET_DEF=asset_def.txt
-grep mqtt ${ASSET_DEF} > /dev/null
-HAS_MQTT=$?
-echo "ASSET_DEF=${ASSET_DEF}"
-if [ ${HAS_MQTT} -eq 0 ]
-then
-    echo "INFO: ACTIVATING MOSQUITTO"
-    mosquitto -c ./config/mosquitto.conf &
-    MQTT_PORT=1883
-    sleep 2
-else
-    MQTT_PORT=
-fi
+export DELTA_MSEC=20
+export MAX_DELAY_MSEC=100
+export GRPC_PORT=50051
+export CORE_IPADDR=127.0.0.1
+export PATH="/usr/local/bin/hakoniwa:${PATH}"
+export LD_LIBRARY_PATH="/usr/local/lib/hakoniwa:${LD_LIBRARY_PATH}"
+export DYLD_LIBRARY_PATH="/usr/local/lib/hakoniwa:${DYLD_LIBRARY_PATH}"
+ASSET_DEF="workspace/runtime/asset_def.txt"
+bash utils/rm_ipcs.bash
 
+HAKO_CONDUCTOR_PID=
+HAKO_ASSET_PROG_PID=
+IS_OCCURED_SIGEVENT="FALSE"
+function signal_handler()
+{
+    IS_OCCURED_SIGEVENT="TRUE"
+    echo "trapped"
+}
+
+function kill_process()
+{
+    echo "trapped"
+    if [ -z "$HAKO_CONDUCTOR_PID" ]
+    then
+        exit 0
+    fi
+    
+    # HAKO_ASSET_PROG_PID に保存されている各PIDをkill
+    for pid in $HAKO_ASSET_PROG_PID; do
+        echo "KILLING: ASSET PROG $pid"
+        kill -s TERM $pid || echo "Failed to kill ASSET PROG $pid"
+    done
+    
+    echo "KILLING: hako-master-rust  $HAKO_CONDUCTOR_PID"
+    kill -9 "$HAKO_CONDUCTOR_PID" || echo "Failed to kill hako-master-rust "
+
+    while [ 1 ]
+    do
+        NUM=$(ps aux | grep hako-master-rust  | grep -v grep | wc -l)
+        if [ $NUM -eq 0 ]
+        then
+            break
+        fi
+        sleep 1
+    done
+
+    exit 0
+}
+
+OLD_PID=`ps aux | grep hako-master-rust  | grep -v grep | awk '{print $2}'`
+if [ -z $OLD_PID ] 
+then
+    :
+else
+    echo "KILLING old pid: ${OLD_PID}"
+    kill -s TERM $OLD_PID
+fi
+trap signal_handler SIGINT
 
 echo "INFO: ACTIVATING HAKO-CONDUCTOR"
-hako-master ${DELTA_MSEC} ${MAX_DELAY_MSEC} ${CORE_IPADDR}:${GRPC_PORT} ${UDP_SRV_PORT} ${UDP_SND_PORT} ${MQTT_PORT} & > /dev/null 
+hako-master-rust  ${DELTA_MSEC} ${MAX_DELAY_MSEC} ${CORE_IPADDR}:${GRPC_PORT} ${UDP_SRV_PORT} ${UDP_SND_PORT} ${MQTT_PORT} &
+HAKO_CONDUCTOR_PID=$!
 
 sleep 1
 
-LAST_PID=
+HAKO_ASSET_PROG_PID=
 for entry in `cat ${ASSET_DEF}`
 do
-    PROG=`echo $entry | awk -F: '{print $1}'`
+    TYPE=`echo $entry | awk -F: '{print $1}'`
+    PROG=`echo $entry | awk -F: '{print $2}'`
     echo "${PROG}" | grep "^#"
     if [ $? -ne 0 ]
     then
         echo "INFO: ACTIVATING $entry"
-        bash $PROG &
-        LAST_PID=$!
+        if [ ${TYPE} = "binary" ]
+        then
+            $PROG &
+            HAKO_ASSET_PROG_PID="$! ${HAKO_ASSET_PROG_PID}"
+        else
+            echo "ERROR: NOT SUPPORTED TYPE=${TYPE}"
+        fi
     fi
     sleep 1
 done
 
-while [ 1 ]
-do
-    sleep 10
+echo "START"
+while true; do
+    echo "Press ENTER to stop..."
+    read input
+    if [ -z "$input" ]; then
+        kill_process
+        break
+    fi
 done
+
+bash utils/rm_ipcs.bash
